77da2dc91139b3fb484412dd204c11adc96d0db7
savinduwannigama <e17369@eng.pdn.ac.lk>
2022-04-20 00:24:30 +0530

    added the Gateway

--- Gateway.java
+++ Gateway.java
@@ -3,6 +3,11 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.util.ArrayList;
 import java.util.List;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.net.SocketException;
+
 
 public class Gateway {
     // ArrayList to store the list of monitors with which TCP connections were already made
@@ -17,10 +22,10 @@ public class Gateway {
 
         // keep receiving broadcast messages and create TCP connections with monitors
         while (true) {
-            // receive broadcast messages
+            // Following methods waits for a new UDP packet
             DatagramPacket recvPacket = recvPacket(recvSocket);
 
-            // get the monitor object from the received packet
+            // reading the monitor onject from the received byte stream
             Monitor monitor = getMonitor(recvPacket);
 
             // get the ip address and port of the monitor
@@ -29,9 +34,9 @@ public class Gateway {
 
 
             // check if the monitor is already in the list
-            if (!monitors.contains(ipAddress + ":" + port)) {
+            if (!rcvdMonitors.contains(ipAddress + ":" + port)) {
                 // add the monitor to the list
-                monitors.add(ipAddress + ":" + port);
+                rcvdMonitors.add(ipAddress + ":" + port);
 
                 // print the monitor information
                 System.out.println("Establishing connection to monitor at: " + ipAddress + ":" + port);
@@ -46,16 +51,49 @@ public class Gateway {
 
     }
 
+    // this methods gets the monitor object from the data stream received
     private static Monitor getMonitor(DatagramPacket recvPacket) {
-        return null;
+        Monitor monitor = null;
+        try {
+            // get the monitor object from the received packet
+            InputStream in = new ByteArrayInputStream(recvPacket.getData());
+            ObjectInputStream ois = new ObjectInputStream(in);
+            monitor = (Monitor) ois.readObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return monitor;
     }
 
+
+    // this methods receives a single datagram packet on the socket given as the argument
     private static DatagramPacket recvPacket(DatagramSocket recvSocket) {
-        return null;
+        DatagramPacket recvPacket = null;
+        try {
+            // creating a byte array as a receiving buffer
+            byte[] recvBuf = new byte[1024];
+
+            // creates a new datagram packet to receive data from the UDP socket
+            recvPacket = new DatagramPacket(recvBuf, recvBuf.length);
+
+            // process will wait here until it gets a new UDP packet
+            recvSocket.receive(recvPacket);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // returns the received UDP packet
+        return recvPacket;
     }
 
-    private static DatagramSocket createRecvSocket(int uDP_RCV_PORT) {
-        return null;
+    // this method creates the Datagram socket to receive all the broadcast messages
+    private static DatagramSocket createRecvSocket(int UDP_RCV_PORT) {
+        DatagramSocket recvSocket = null;
+        try {
+            recvSocket = new DatagramSocket(UDP_RCV_PORT);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+        return recvSocket;
     }
-    
 }
